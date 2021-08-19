da31128789ffb25cf3ddb40b2cce1cfd6dc586ca
Ayush Pandey <ayush.pandey@daffodilsw.com>
2021-08-19 22:57:28 +0530

    updates

--- src/ActivityIndicator.js
+++ src/ActivityIndicator.js
@@ -1,13 +1,25 @@
 import { ReactComponent as Indicicator } from "./ActivityIndicator.svg";
 const ActivityIndicator = (props) => {
-  const { animating = true, color = "rgb(29, 161, 242)", size } = props || {};
+  const {
+    animating = true,
+    style,
+    color = "rgb(29, 161, 242)",
+    size,
+  } = props || {};
   if (!animating) {
     return null;
   }
 
-  let indicatorSize = size === "large" ? 80 : 40;
+  const { transform } = style || {};
+
+  let indicatorSize = size === "large" ? 36 : 20;
   return (
-    <Indicicator width={indicatorSize} height={indicatorSize} fill={color} />
+    <Indicicator
+      fill={color}
+      width={indicatorSize}
+      height={indicatorSize}
+      style={{ transform, margin: "auto" }}
+    />
   );
 };
 
