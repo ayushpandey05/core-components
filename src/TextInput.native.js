6350e0903750400768511247ea8ddee9385dc4ef
Ayush Pandey <ayush.pandey@daffodilsw.com>
2021-08-20 22:29:11 +0530

    updates

--- src/TextInput.native.js
+++ src/TextInput.native.js
@@ -1,3 +1,16 @@
-import { TextInput } from "react-native";
+import React from "react";
+import { TextInput as RNTextInput } from "react-native";
+import { runFunction } from "./Utility";
+
+const TextInput = (props) => {
+  return (
+    <RNTextInput
+      {...props}
+      ref={(ref) => {
+        runFunction(props?.getRef, ref);
+      }}
+    />
+  );
+};
 
 export default TextInput;
